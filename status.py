e75be50842235f9b5fd2cd72c813d232275a9ca5
ArchbishopFlabrus <matthewdconnell@outlook.com>
2017-01-24 01:27:47 -0600

    Pep nit picks
    Signed-off-by: ArchbishopFlabrus <matthewdconnell@outlook.com>

--- status.py
+++ status.py
@@ -1,18 +1,18 @@
 # Imports
 from sys import platform, argv
 from os.path import join, expanduser
-from xml.etree import ElementTree as ET
+from xml.etree import ElementTree as Et
 
 # xpath black magic
 status_path = ".//*[@name='" + argv[1] + "']/message"
 
 # Always get the correct file path.
-if (platform == 'win32'):
-    filepath = join( expanduser('~'), 'AppData', 'Roaming', '.purple', 'status.xml' )
+if platform == 'win32':
+    filepath = join(expanduser('~'), 'AppData', 'Roaming', '.purple', 'status.xml')
 else:
-    filepath = join( expanduser('~'), '.purple', 'status.xml' )
+    filepath = join(expanduser('~'), '.purple', 'status.xml')
 
 # Parse the XML tree, get our node, update it's text, write the file.
-tree = ET.parse(filepath)
+tree = Et.parse(filepath)
 tree.getroot().find(status_path).text = str(argv[2])
 tree.write(filepath)
